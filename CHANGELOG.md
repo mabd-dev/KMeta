48d22437e482e2bc0afbdd5feddd8fcffe0d1ad2
MABD <mabd.universe@gmail.com>
2026-05-23 11:59:49 +0300

    added changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# CHANGELOG
+
+All notable changes to this project will be documented in this file.
+
+---
+
+## Unreleased
+
+### Updates
+
+- feat/supportNestedInterfaces: if `@loggable` is applied to interface that have one or more super types
+
+## [0.1.0] - 2025-11-09
+
+Initial Release
+
+- Supported annotations: `@Loggable` + `@Copy` + `@ToNiceString`
--- src/main/kotlin/kmeta/loggable/Main2.kt
+++ src/main/kotlin/kmeta/loggable/Main2.kt
@@ -3,21 +3,19 @@ package kmeta.loggable
 import com.mabd.kmeta.loggable.Loggable
 
 sealed interface Shape {
-
     @Loggable
-    interface Polygon: Shape {
+    interface Polygon : Shape {
         fun calculateArea(): Double
     }
 }
 
 sealed interface Interface1 {
-   sealed interface Interface2: Interface1 {
-
-       @Loggable
-       interface Test: Interface2 {
-           fun doSomething()
-       }
-   }
+    sealed interface Interface2 : Interface1 {
+        @Loggable
+        interface Test : Interface2 {
+            fun doSomething()
+        }
+    }
 }
 
 fun main() {
